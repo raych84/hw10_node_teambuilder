e2efcc45d70e726f4ffb7e85ce4d2931a7913b33
Rachel Williams <rachel_wms@hotmail.com>
2020-11-03 23:11:39 -0500

    having issues getting answers to write to html file, pushing up what I have. will continue later. WIP

--- app.js
+++ app.js
@@ -5,102 +5,147 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
+const OUTPUT_DIR = path.resolve(__dirname, "./output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+// used htmlRender.js for ref
+const teamMembers = [];
+const idArray = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+// ref week 5 day 1,2
+function buildGreatness() {
+	inquirer
+		.prompt([{
+			name: "action",
+			type: "list",
+			message: "What type of employee would you like add?",
+			choices: [
+				"Add Manager?",
+				"Add Engineer?",
+				"Add Intern?",
+				"Exit."
+			]
 
-function buildNewTeam(){
+		}])
+		// got this from hw 12
+		.then(function (answers) {
+			switch (answers.action) {
+				case "Add Manager?":
+					addManager();
+					break;
 
+				case "Add Engineer?":
+					addEngineer();
+					break;
+
+				case "Add Intern?":
+					addIntern();
+					break;
+
+					case "Exit":
+					return;
+			}
+		})
+
+
+
+function addManager() {
+	inquirer
+		.prompt([
+			{
+				type: "input",
+				message: "What is the manager's name?",
+				name: "managerName"
+			},
+			{
+				type: "number",
+				message: "What is the manager's id number?",
+				name: "managerId"
+			},
+			{
+				type: "input",
+				message: "What is the manager's email address?",
+				name: "managerEmail"
+			},
+			
+			{
+				type: "number",
+				message: "What is the manager's office number?",
+				name: "officeNumber"
+			}
+		]).then(answers => {
+			const manager = new Manager(answers.managerName, answers.interId, answers.managerEmail, answers.officeNumber);
+			teamMembers.push(manager);
+			idArray.push(answers.managerId);
+			buildNewTeam();
+		});
 }
-
-
-
-function addManager(){
+function addEngineer() {
 	inquirer
-	.prompt([
-	  {
-		type: "input",
-		message: "What is the manager's name?",
-		name: "managerName"
-	  },
-	  {
-		type: "number",
-		message: "What is the manager's id number?",
-		name: "managerId"
-	  },
-	  {
-		type: "input",
-		message: "What is the manager's email address?",
-		name: "managerEmail"
-	  },
-	  {
-		type: "number",
-		message: "What is the manager's office number?",
-		name: "officeNumber"
-	  }
-	])
-	
-		
-};
-
-function addEngineer(){
+		.prompt([
+			{
+				type: "input",
+				message: "What is the engineer's name?",
+				name: "engineerName"
+			},
+			{
+				type: "number",
+				message: "What is the engineers's id number?",
+				name: "engineerId"
+			},
+			{
+				type: "input",
+				message: "What is the engineer's email address?",
+				name: "engineerEmail"
+			},
+			{
+				type: "input",
+				message: "What is the engineer's gitHub username?",
+				name: "github"
+			}
+		]).then(answers => {
+			const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.gitHub)
+			teamMembers.push(engineer);
+			idArray.push(answers.engineerId);
+			buildNewTeam();
+		});
+}
+function addIntern() {
 	inquirer
-	.prompt([
-	  {
-		type: "input",
-		message: "What is the engineer's name?",
-		name: "engineerName"
-	  },
-	  {
-		type: "input",
-		message: "What is the engineer's gitHub username?",
-		name: "gitHubName"
-	  },
-	  {
-		type: "number",
-		message: "What is the engineers's id number?",
-		name: "engineerId"
-	  },
-	  {
-		type: "input",
-		message: "What is the engineer's email address?",
-		name: "engineerEmail"
-	  }
-	  
-	])
+		.prompt([
+			{
+				type: "input",
+				message: "What is the intern's name?",
+				name: "internName"
+			},
+			{
+				type: "number",
+				message: "What is the intern's id number?",
+				name: "internId"
+			},
+			{
+				type: "input",
+				message: "What is the intern's email address?",
+				name: "internEmail"
+			},			
+			{
+				type: "input",
+				message: "What is the intern's school name?",
+				name: "schoolName"
+			},
 
-};
-function addIntern(answers){
-	inquirer
-	.prompt([
-	  {
-		type: "input",
-		message: "What is the intern's name?",
-		name: "internName"
-	  },
-	  {
-		type: "input",
-		message: "What is the intern's school name?",
-		name: "schoolName"
-	  },
-	  {
-		type: "number",
-		message: "What is the intern's id number?",
-		name: "internId"
-	  },
-	  {
-		type: "input",
-		message: "What is the intern's email address?",
-		name: "internEmail"
-	  }
-	]).then(answers) => {
-		const intern = new Intern(answer.interName, answer.schoolName, answer.interId, answer.internEmail)
-	}
+			// tutor provide...need to understand how this is valid
+		]).then(answers => {
+			const intern = new Intern(answers.interName, answers.interId, answers.internEmail, answers.schoolName)
+			teamMembers.push(intern);
+			idArray.push(answers.internId);
+			buildNewTeam();
+		});
+}
 }
 
 
@@ -108,20 +153,18 @@ function addIntern(answers){
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-
-fs.writeFileSync(outhPath, render(teamMemebers), "utf-8");
+// tutor provided
+function buildNewTeam() {
+	fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
 }
+
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-const createFile = (filename, extension) => {
-	const filePath = `${process.cwd()}/${filename}.${extension}`
-	shell.touch(filePath);
-	return filePath;
-  };
+buildGreatness()
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
